3748706d6fdd284a04a4834a3b3765465fb73821
coihsan <110887888+coihsan@users.noreply.github.com>
2024-01-24 09:51:48 +0000

    b

---
+++ app/login/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useFormState, useFormStatus } from "react-dom";
+export default function LoginPage() {
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+
+  return (
+    <form action={dispatch}>
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <div>{errorMessage && <p>{errorMessage}</p>}</div>
+      <LoginButton />
+    </form>
+  );
+}
+
+function LoginButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button aria-disabled={pending} type="submit">
+      Login
+    </button>
+  );
+}
